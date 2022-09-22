81a997c71c107ae98eb24a3740a64b7d36660bd2
Duarte Sa morais <dsa-mora@c1r11s4.42lisboa.com>
2022-09-22 20:02:58 +0100

    added things

--- Exam2Prep/itoa.c
+++ Exam2Prep/itoa.c
@@ -6,7 +6,7 @@
 /*   By: dsa-mora <dsa-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 19:08:23 by dsa-mora          #+#    #+#             */
-/*   Updated: 2022/09/22 19:42:05 by dsa-mora         ###   ########.fr       */
+/*   Updated: 2022/09/22 20:01:13 by dsa-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,46 @@ int	ft_size(int nbr)
 	int	i;
 
 	i = 0;
-	if (nbr < 10)
-		return (i + 1);
-	while (nbr > 10)
+	if (nbr < 10 && nbr > 0)
+		return (i + 2);
+	while (nbr > 10 || nbr < 0)
 	{
+		if (nbr < 0)
+		{
+			i++;
+			nbr *= -1;
+		}
 		nbr = nbr / 10;
 		i++;
 	}
 	return (i + 1);
 }
 
-void	ft_putnbr(int nbr, char *str_nbr, int i)
+void	ft_putnbr(int nbr, char *str_nbr, int *i)
 {
 	if (nbr == INT_MIN)
 	{
-		str_nbr[i] = '-';
-		i++;
+		str_nbr[*i] = '-';
+		(*i)++;
 		ft_putnbr(2, str_nbr, i);
 		ft_putnbr((147483648), str_nbr, i);
 	}
 	if (nbr < 0)
 	{
-		str_nbr[i] = '-';
+		str_nbr[*i] = '-';
 		nbr = nbr * (-1);
-		i++;
+		(*i)++;
 		ft_putnbr(nbr, str_nbr, i);
 	}
 	if (nbr > 9)
 	{
-		ft_putnbr(nbr / 10, str_nbr, i++);
+		ft_putnbr(nbr / 10, str_nbr, i);
 		ft_putnbr(nbr % 10, str_nbr, i);
 	}
 	else
 	{
-		str_nbr[i] = nbr + '0';
-		i++;
+		str_nbr[*i] = nbr + '0';
+		(*i)++;
 	}
 }
 
@@ -62,9 +67,11 @@ char	*ft_itoa(int nbr)
 {
 	char	*str_nbr;
 	int		size;
-	int		i;
+	int		*i;
+	int		a;
 
-	i = 0;
+	a = 0;
+	i = &a;
 	size = ft_size(nbr);
 	printf("%i\n", size);
 	str_nbr = (char *)malloc(size * sizeof(char));
@@ -74,5 +81,5 @@ char	*ft_itoa(int nbr)
 
 int main(void)
 {
-	printf("%s", ft_itoa(123));
+	printf("%s", ft_itoa(-123));
 }
